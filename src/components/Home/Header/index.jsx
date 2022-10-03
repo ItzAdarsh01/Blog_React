276f1899d09f279f2bec722d8b1df77da6a3ba3d
adarsh <adarsh171101@gmail.com>
2022-10-03 09:53:26 +0530

    first commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h1>Blogs</h1>
+    <p>
+     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, vitae perspiciatis excepturi et facilis nemo repudiandae delectus facere? Vel, corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eum!
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,87 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'connecting with spirituality',
+    category: 'tour',
+    subCategory: ['myotur', 'view', 'nature'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Adarsh kasaudhan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: '30 September, 2022',
+    cover: '/assets/images/designer-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'let the travel begin',
+    category: 'travel',
+    subCategory: ['vacation', 'holidays', 'sight seeing'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Adarsh kasaudhan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: '25 September, 2022',
+    cover: '/assets/images/f67396fc3cfce63a28e07ebb35d974ac.jpg',
+  },
+  {
+    id: 3,
+    title: 'what is virtual reality?',
+    category: 'virtual reality',
+    subCategory: ['vr', 'new world', 'awesome'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Adarsh kasaudhan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: '20 September, 2022',
+    cover: '/assets/images/fQwuyKJ9qxjSbr6REcgtmW-1200-80.jpg',
+  },
+  {
+    id: 4,
+    title: 'exploring 3d view',
+    category: '3d view',
+    subCategory: ['new eyes', 'fun', 'others'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Adarsh kasaudhan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: '15 September, 2022',
+    cover: '/assets/images/graphic-design-trends.png',
+  },
+  {
+    id: 5,
+    title: 'take a tour',
+    category: 'tour',
+    subCategory: ['tour', 'exploring', 'lifestyle'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Adarsh kasaudhan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: '10 september, 2022',
+    cover: '/assets/images/make-it-personal.jpg',
+  },
+  {
+    id: 6,
+    title: 'more travel more fun',
+    category: 'travel',
+    subCategory: ['beaches', 'sea', 'holidays'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Adarsh kasaudhan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 09, 2022',
+    cover:
+      '/assets/images/Purple-Combination-colors-graphic-design-predictions-1024x576-1024x576.jpg',
+  },
+  {
+    id: 7,
+    title: 'finding love in art',
+    category: 'art',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Adarsh kasaudhan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'February 15, 2022',
+    cover: '/assets/images/Synthwave-Postmodern.jpg',
+  },
+];
