352e12bb729b989c45ff968a6d5bcd2fa8538183
adarsh <adarsh171101@gmail.com>
2022-10-11 13:22:22 +0530

    added newsletter

--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -3,10 +3,14 @@ import './styles.css';
 
 const Header = () => (
   <header className='home-header'>
-    <h1>Blogs</h1>
+    <h1>Blogger.com</h1>
     <p>
-     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, vitae perspiciatis excepturi et facilis nemo repudiandae delectus facere? Vel, corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eum!
+     Lorem ipsum dolor sit, amet consectetur adipisicing ilis nemo repudiandae delectus facere? Vel, corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eum!
     </p>
+    <div className='myspan'>
+    <span>To get regular updates subscribe us at:</span><a href='https://adarsh200kasaudhan.herokuapp.com/' 
+    target='blank'>Blogger</a>
+    </div>
   </header>
 );
 
